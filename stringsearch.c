75037b4dd5518ddcda2b78ec2cc6175109de076b
mahan <mahan.mohammadi.a@gmail.com>
2025-01-06 17:12:01 +0330

    fixed number issues

--- stringsearch.c
+++ stringsearch.c
@@ -3,29 +3,28 @@
 int main() {
     char str[1000], word[10];
     int wordCount = 0, letterCount = 0, match = 0;
-
     printf("Enter a string: ");
     gets(str);
-
     printf("Enter the word you want to search: ");
     gets(word);
-
-
     int i = 0, j, k;
     while (str[i] != '\0') {
-
         if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z')) {
             letterCount++;
         }
-
         if ((str[i] != ' ' && str[i] != '\t') &&
             (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t')) {
-            wordCount++;
+            int temp = i;
+            while (str[temp] != ' ' && str[temp] != '\t' && str[temp] != '\0') {
+                if ((str[temp] >= 'a' && str[temp] <= 'z') || (str[temp] >= 'A' && str[temp] <= 'Z')) {
+                    wordCount++;
+                    break;
+                }
+                temp++;
+            }
         }
-
         i++;
     }
-
     i = 0;
     while (str[i] != '\0') {
         for (j = i, k = 0; word[k] != '\0' && str[j] == word[k]; j++, k++);
@@ -33,13 +32,10 @@ int main() {
             (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t')) {
             match++;
         }
-
         i++;
     }
-
     printf("Number of words: %d\n", wordCount);
     printf("Number of letters: %d\n", letterCount);
     printf("Occurrences of the word '%s': %d\n", word, match);
-
     return 0;
 }
