2d78afdf5429f6add6b570d0241ad7f0f2b64f5b
mahan <mahan.mohammadi.a@gmail.com>
2025-01-09 19:41:25 +0330

    new

---
+++ printoddnumbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>  // Include the standard input/output header file.
+
+void main() {
+    int i, n, sum = 0;  // Declare variables 'i' for loop counter, 'n' for user input, and 'sum' to store the sum.
+
+    printf("Input number of terms : ");  // Print a message to prompt user input.
+    scanf("%d", &n);  // Read the value of 'n' from the user.
+    printf("\nThe odd numbers are :");  // Print a message indicating that odd numbers will be displayed.
+
+    for (i = 1; i <= n; i++) {  // Start a loop to generate odd numbers based on user input.
+        printf("%d ", 2 * i - 1);  // Print the odd number.
+        sum += 2 * i - 1;  // Add the odd number to the running sum.
+    }
+
+    printf("\nThe Sum of odd Natural Number upto %d terms : %d \n", n, sum);  // Print the sum of odd numbers.
+
+}
