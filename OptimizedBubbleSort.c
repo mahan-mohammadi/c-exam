5ae533a534ed3906146e5794831ad938fb071293
mahan <mahan.mohammadi.a@gmail.com>
2025-01-12 19:34:25 +0330

    a more optimized version of bubble sort

---
+++ OptimizedBubbleSort.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#define SIZE 10
+
+int main(){
+	int a[] = {10,9,8,7,6,5,4,3,2,1};
+	for(int i =0 ; i<SIZE ; i++){
+		for(int j =0; j<SIZE -i -1; j++){
+			if(a[j]>a[j+1]){
+				int temp;
+				temp = a[j];
+				a[j] = a[j+1];
+				a[j+1] = temp;
+			}
+		}
+	}
+	for(int i=0 ; i<SIZE ; i++){
+		printf("%d ", a[i]);
+	}
+	}
+
